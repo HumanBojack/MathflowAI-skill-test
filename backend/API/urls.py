5d4373199ad8e42a6342d8c3ffe62eba0c648d32
HumanBojack <spychalar59@gmail.com>
2023-09-23 22:52:36 +0200

    feat: /answer route to answer a question and handle the buffer behavior

--- backend/API/urls.py
+++ backend/API/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("random/", views.get_random_question, name="get-random-question"),
+    path("answer/", views.post_answer, name="post-answer"),
     path("money/<int:user_id>", views.get_money_buffer, name="get-money-buffer"),
 ]
--- backend/API/views.py
+++ backend/API/views.py
@@ -1,10 +1,10 @@
-from datetime import datetime
+from datetime import datetime, time
 
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Question, Answer, User
-from API.serializers import QuestionSerializer
+from API.serializers import QuestionSerializer, AnswerSerializer
 
 
 @api_view(["GET"])
@@ -14,6 +14,51 @@ def get_random_question(request):
     # Serialize the question
     serializer = QuestionSerializer(question)
     # Return the serialized question
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def post_answer(request):
+    # Get the question_id and user_id from the request data
+    question_id = request.data.get("question_id")
+    user_id = request.data.get("user_id")
+    try:
+        # Get the question and user
+        question = Question.objects.get(id=question_id)
+        user = User.objects.get(id=user_id)
+    except (Question.DoesNotExist, User.DoesNotExist):
+        return Response(
+            {"error": "Question or User not found"}, status=status.HTTP_404_NOT_FOUND
+        )
+    # Get the answer from the request data
+    answer = request.data.get("answer")
+    # Create the answer
+    answer = Answer.objects.create(question=question, user=user, answer=answer)
+
+    # Update the user's money if the user has answered 10 questions today
+    answers = Answer.objects.filter(user=user, created_at__date=datetime.now().date())
+    if len(answers) == 10:
+        # Get the total amount of money from the correct_answers
+        money = sum(
+            [
+                answer.question.money_value
+                for answer in answers
+                if answer.answer == answer.question.answer
+            ]
+        )
+        # Update the user's money
+        user.money += money
+        user.save()
+
+    # Delete the user's answers that were created before today (at 00:00)
+    answers = Answer.objects.filter(
+        user=user, created_at__lt=datetime.combine(datetime.now().date(), time.min)
+    )
+    answers.delete()
+
+    # Serialize the answer
+    serializer = AnswerSerializer(answer)
+    # Return the serialized answer
     return Response(serializer.data)
 
 
